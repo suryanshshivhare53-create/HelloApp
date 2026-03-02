e887a9b9afcf7f0add7c964c5aaee0c3ccdc5055
Suryansh Shivhare <suryanshshivhare53@gmail.com>
2026-03-02 09:54:03 +0530

    w1

---
+++ Level1Ques4.java
@@ -0,0 +1,18 @@
+
+public class Level1Ques4 {
+
+    public static void main(String[] args) {
+        int costPrice = 129;
+        int sellingPrice = 191;
+
+        int profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / (double) costPrice) * 100;
+
+        System.out.println(
+                "The Cost Price is INR " + costPrice
+                + " and Selling Price is INR " + sellingPrice + "\n"
+                + "The Profit is INR " + profit
+                + " and the Profit Percentage is " + profitPercentage
+        );
+    }
+}
