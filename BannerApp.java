20233ba1797f3e753ec2b4809eb1e5e5c0e5bc7e
Suryansh Shivhare <suryanshshivhare53@gmail.com>
2026-03-11 23:21:58 +0530

    added:uc8

---
+++ BannerApp.java
@@ -0,0 +1,66 @@
+
+import java.util.HashMap;
+
+public class BannerApp {
+
+    // Creates and initializes character pattern map
+    public static HashMap<Character, String[]> createCharacterMap() {
+
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        charMap.put('O', new String[]{
+                "  ***     ",
+                " ** **    ",
+                "**   **   ",
+                "**   **   ",
+                "**   **   ",
+                " ** **    ",
+                "  ***     "
+        });
+
+        charMap.put('P', new String[]{
+                " *****   ",
+                " **  **  ",
+                " **   ** ",
+                " *****   ",
+                " **      ",
+                " **      ",
+                " **      "
+        });
+
+        charMap.put('S', new String[]{
+                " ***** ",
+                " **    ",
+                " **    ",
+                " ***   ",
+                "   **  ",
+                "   **  ",
+                " ***** "
+        });
+
+        return charMap;
+    }
+
+    // Renders the banner using nested loops
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+
+        int height = charMap.get('O').length;
+
+        for (int line = 0; line < height; line++) {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+                sb.append(charMap.get(ch)[line]).append(" ");
+            }
+
+            System.out.println(sb);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        HashMap<Character, String[]> charMap = createCharacterMap();
+        displayBanner("OOPS", charMap);
+    }
+}
